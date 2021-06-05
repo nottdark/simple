a651db562e7d3c2060549b8fb6658ac9b6f4ee5c
nottdark <nottdark.dev@gmail.com>
2021-06-05 23:21:35 +0700

    Revert "Add route /user"
    This reverts commit 054b0dcc275ccf11bdbc898769ac45e6c3a59786.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3003
 const { hello } = require('./helpers/index')
-const { user } = require('./helpers/user')
 
 app.get('/', (req, res) => {
   res.send({ 
@@ -13,12 +12,6 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send({ 
     msg: hello()
-  })
-})
-
-app.get('/user', (req, res) => {
-  res.send({ 
-    msg: user()
   })
 })
 
