054b0dcc275ccf11bdbc898769ac45e6c3a59786
nottdark <nottdark.dev@gmail.com>
2021-06-05 23:15:52 +0700

    Add route /user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3003
 const { hello } = require('./helpers/index')
+const { user } = require('./helpers/user')
 
 app.get('/', (req, res) => {
   res.send({ 
@@ -12,6 +13,12 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send({ 
     msg: hello()
+  })
+})
+
+app.get('/user', (req, res) => {
+  res.send({ 
+    msg: user()
   })
 })
 
