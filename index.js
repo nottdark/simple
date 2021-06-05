70f4db0b38c0f9e2a6a2fe2f714f4edf813eb716
nottdark <nottdark.dev@gmail.com>
2021-06-05 23:31:08 +0700

    Add route /greet

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3003
 
+const { hello } = require('./utils')
+
 app.get('/', (req, res) => {
   res.send({ 
     status: trues
+  })
+})
+
+app.get('/greet', (req, res) => {
+  res.send({ 
+    text: hello()
   })
 })
 
